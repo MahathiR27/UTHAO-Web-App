5b149673080a9034761d68a1b9370d2530411818
SajimRZ <the8bitcrow@gmail.com>
2025-11-20 21:17:16 +0600

    restaurant frontend page + form submit

--- backend/src/routes/restaurantRoutes.js
+++ backend/src/routes/restaurantRoutes.js
@@ -3,6 +3,6 @@ import { createRestaurant } from '../controllers/restaurantController.js';
 
 const router = express.Router();
 
-router.post("/restaurantsignup", createRestaurant);
+router.post("/create-restaurant", createRestaurant);
 
 export default router;
